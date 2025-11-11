cf58b940e18532186ed1101097b57d24f7ce516e
mehakx <mehak.garg037@gmail.com>
2025-11-11 14:47:17 -0500

    Refactor Vite config to use defineConfig and update base

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default {
-  base: process.env.REPO_NAME,
-  plugins: [
-    react(),
-  ],
-}
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react( )],
+  base: '/assignment33/'
+})
